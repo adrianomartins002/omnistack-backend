bb06c1eaa03b311b7f94376b2b3fc5f6e6fb58a0
Adriano <adrianomartins002@gmail.com>
2020-01-14 18:59:53 -0300

    feat(start): start the project

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-k9a9f.mongodb.net/week10?retryWrites=true&w=majority',{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+});
+
+
+//Métodos http: get, post, put, delete
+
+// Tipos de parâmetros:
+// Query params: req.query(Filtros, ordenacao, paginacao, ...) para acessar os parametros listados request.query
+
+// Route params: (atualizacao etc) para acessar é request.params(identificar um recurso na alteração ou remoção)
+// Body
+
+app.listen(3333)
