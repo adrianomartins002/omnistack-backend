17723bd907c9f4f03b28a94747974286a294abdf
Adriano <adrianomartins002@gmail.com>
2020-01-15 09:41:02 -0300

    feat(put,delete devs): add functions by delete and put dev

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -23,10 +23,29 @@ module.exports = {
 
         return response.json({devs});
     },
-    async update(){
 
-    },
-    async destroy(){
+    async update(request, response){
+        const github_username = request.params.github_username
+        const {latitude, longitude, techs, bio} = request.body
         
+        const techsArray = parseStringAsArray(techs);
+        
+        const location = {
+            type:'Point',
+            coordinates: [longitude, latitude],
+        }
+        await Dev.update({github_username},{location, techs: techsArray, bio})
+        const devBuscado = await Dev.findOne({github_username})
+        
+
+        response.json({devBuscado})
+    },
+
+    async destroy(request, response){
+        const github_username = request.params.github_username
+        await Dev.deleteOne({github_username}, function (err){
+            if(err) return err
+            else response.send("deletado com sucesso")
+        })
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,6 @@ routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store)
 
 routes.get('/search', SearchController.index )
-
+routes.put('/search/:github_username', SearchController.update )
+routes.delete('/search/:github_username', SearchController.destroy )
 module.exports = routes;
